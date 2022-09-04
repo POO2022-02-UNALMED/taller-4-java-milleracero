d5c5d7170fadb392c5b92bdb347371578a8bceab
milleracero <mchicaa@unal.edu.co>
2022-09-04 14:47:12 -0500

    1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,12 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+	long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
@@ -31,9 +30,12 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula=1;
         totalPersonas++;
     }
-    
+    Persona() {
+       this(null,0);
+    }
     long getCedula() {
         return cedula;
     }
